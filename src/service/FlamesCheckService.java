d1ca0607b17184fc3190d79ca997754ef9c44b64
nayankela <nayankela09@gmail.com>
2020-08-25 12:42:27 +0530

    done

--- src/service/FlamesCheckService.java
+++ src/service/FlamesCheckService.java
@@ -7,3 +7,43 @@ package service;
 // You must return only the following values ['f','l','a','m','e','s']
 // change the return value at the end of the method corresponding to your return value
 
+public class FlamesCheckService {
+
+	public char findFlames(String name1, String name2) {
+
+		int len1 = name1.length();
+		int len2 = name2.length();
+		char str1[] = name1.toCharArray();
+		char str2[] = name2.toCharArray();
+		String flm = "flames";
+		for (int i = 0; i < len1; i++) {
+			for (int j = 0; j < len2; j++) {
+				if (str1[i] == str2[j]) {
+					str1[i] = '*';
+					str2[j] = '*';
+					break;
+				}
+			}
+		}
+		String tname = new String(str1);
+		tname = tname + (new String(str2));
+		tname = tname.replace("*", "");
+		int num = 0, unmatchedWords = 0;
+		unmatchedWords = tname.length();
+		System.out.println("Umatched Words are:- "+unmatchedWords);
+
+		for (int s = 6; s >= 2; s--) {
+			if (unmatchedWords > s) {
+				num = unmatchedWords - s;
+			} else {
+				num = unmatchedWords;
+			}
+			while (num > s) {
+				num = num - s;
+			}
+			flm = flm.substring(num, flm.length()) + (flm.substring(0, num - 1));
+		}
+		System.out.println("Your Name:\n" + name1 + "\nYour Crush Name:\n" + name2 + "\nYour Result:\n" + flm.charAt(0));
+		return flm.charAt(0);
+	}
+}
